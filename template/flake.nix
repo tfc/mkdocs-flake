03d063b50ed0e90dfa9c5b9ba2fc1c8a9af1a2a3
Jacek Galowicz <jacek@galowicz.de>
2024-03-27 11:09:43 +0100

    Add example template

---
+++ template/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # (1) add mkdocs-flake input
+    mkdocs-flake.url = "github:applicative-systems/mkdocs-flake";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    # (2) import mkdocs-flake module
+    imports = [
+      inputs.mkdocs-flake.flakeModules.default
+    ];
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+    perSystem = { config, self', inputs', pkgs, system, ... }: {
+      packages.default = pkgs.hello;
+
+      # (3) point mkdocs-flake to your mkdocs root folder
+      documentation.mkdocs-root = ./docs;
+
+      # (4) packages.${system}.documentation is generated for you.
+      #     build it with `nix build .#documentation`
+    };
+  };
+}
